8916590270cbf045d35dd47716509a69aa33bb4c
飘 <hairong560@126.com>
2015-12-29 17:44:46 +0800

    Focus html
    增加了html结构和css

---
+++ js/focus.js
@@ -0,0 +1,16 @@
+$(function(){
+    var box=$(".focus-wrap");
+    var boxCon=$(".focus-wrap ul");
+    var item=$(".focus-wrap ul li");
+    var nItem=item.length;
+    var boxWidth=box.width();
+    var ulWidth=wLeft*nItem;
+    var btnN=$(".focus-wrap .btn-next");
+    var btnP=$(".focus-wrap .btn-prev");
+    var wLeft=boxCon.css("left");
+    boxCon.css({'width':ulWidth+'px'})
+    btnN.click(function(){
+        
+    })
+    
+})
